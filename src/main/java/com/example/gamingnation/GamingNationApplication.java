f6fef4ef351ebee17cc1bf9009d61c8a75419578
nicolasPaiva <nicolaspaiva@outlook.com>
2024-05-03 22:39:23 +0100

    first commit

---
+++ src/main/java/com/example/gamingnation/GamingNationApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamingnation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingNationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingNationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gamingnation/GamingNationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamingnation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingNationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
